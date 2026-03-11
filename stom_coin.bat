7b1c2628a18e01528c6b96e3ec79f93fd24c1fec
CI Park <parkchanil77@naver.com>
2026-03-11 18:14:49 +0900

    feat: add STOM dual virtualenv setup and launchers

--- stom_coin.bat
+++ stom_coin.bat
@@ -1,7 +1,18 @@
 @echo off
+chcp 65001 >nul
+title STOM Coin (Virtual Environment)
+
+set VENV_64BIT=%~dp0venv_64bit
+set PYTHON_64BIT=%VENV_64BIT%\Scripts\python.exe
+
+if not exist "%PYTHON_64BIT%" (
+    echo [ERROR] 64-bit virtual environment is missing.
+    echo [INFO] Run setup_stom.bat first.
+    pause
+    exit /b 1
+)
 
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
@@ -10,7 +21,7 @@ if '%errorlevel%' NEQ '0' (
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
@@ -19,6 +30,21 @@ if '%errorlevel%' NEQ '0' (
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
-    python ./utility/database_check.py
-    python stom.py coin
+
+    "%PYTHON_64BIT%" -c "import PyQt5, PyQt5.QtWebEngineWidgets, zmq, pyttsx3, websockets, cryptography, talib" >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo [ERROR] 64-bit virtual environment is incomplete.
+        echo [INFO] Run setup_stom.bat again.
+        pause
+        exit /b 1
+    )
+
+    "%PYTHON_64BIT%" ./utility/database_check.py
+    if %errorlevel% neq 0 (
+        echo [ERROR] Database check failed.
+        pause
+        exit /b 1
+    )
+
+    "%PYTHON_64BIT%" stom.py coin %*
     pause
